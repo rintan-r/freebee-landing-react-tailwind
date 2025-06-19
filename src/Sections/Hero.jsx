f6bc1ed7c56847e30ba04b90398875290bf02a99
rintan-r <dev.rintanrenatta@gmail.com>
2025-06-19 15:09:37 +0700

    button fix in hero section

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -28,7 +28,14 @@ function Hero() {
                         <Chip key={index} label={item} />
                     ))}
                 </div>
-                <CTAbutton />
+                <CTAbutton
+                    variant="primary"
+                    size="lg"
+                    whileHover={{ scale: 1.1, rotate: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    Lorem Ipsum
+                </CTAbutton>
             </div>
             <div className="w-full pl-2.5 pr-4 justify-center">
                 <img src={draw} alt="Hero Illustration" className="w-full h-auto" />
